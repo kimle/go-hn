d08f07afe0b8cb88b9812ebeaf4ca77b23a26326
Kim Le <kle@abo.fi>
2018-02-22 07:45:58 +0200

    added GetIds

--- cmd/client.go
+++ cmd/client.go
@@ -1,40 +1,55 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"io/ioutil"
+	"flag"
+	"fmt"
 	"log"
-	"net/http"
+	"os"
 
 	pb "github.com/kimle/go-hackernews/service"
 	"google.golang.org/grpc"
 )
 
-const (
-	// address = "localhost:50051"
-	address = "35.229.113.24:50051"
-)
+var flagvar int
+var env string
+var address string
+
+func init() {
+	flag.IntVar(&flagvar, "stories", 0, "n stories to fetch and display")
+	flag.StringVar(&env, "env", "dev", "enviroment to use")
+	flag.Parse()
+	if flagvar == 0 {
+		fmt.Println("you must provide at least one story!")
+		os.Exit(0)
+	}
+
+	if env == "dev" {
+		address = "localhost:50051"
+	} else if env == "prod" {
+		address = "35.229.113.24:50051"
+	}
+}
 
 func main() {
-	allIds := make([]int32, 500)
-	stories := make([]*pb.Story, 10)
-	resp, _ := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	response := bytes.NewReader([]byte(string(body)))
-	json.NewDecoder(response).Decode(&allIds)
-	for i := 0; i < 10; i++ {
-		stories[i] = &pb.Story{Id: allIds[i]}
-	}
+	stories := make([]*pb.Story, flagvar)
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+
 	defer conn.Close()
 	c := pb.NewTestClient(conn)
-	log.Printf("argument: %v", &pb.TopStories{TopStories: stories})
-	r, err := c.GetStories(context.Background(), &pb.TopStories{TopStories: stories})
-	log.Printf("%v", r)
+	r, err := c.GetIds(context.Background(), &pb.Amount{Amount: int32(flagvar)})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < flagvar; i++ {
+		stories[i] = &pb.Story{Id: r.Ids[i]}
+	}
+	rStories, err := c.GetStories(context.Background(), &pb.TopStories{TopStories: stories})
+	for i, story := range rStories.Stories {
+		fmt.Printf("%d. %v\n", i, story)
+	}
 }
